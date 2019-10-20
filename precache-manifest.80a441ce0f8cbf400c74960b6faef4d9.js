7b83044a91dd981cae71251592b3427081fec79e
sg19repos <sg19repos@gmail.com>
2019-10-20 22:11:30 +0530

    Updates

---
+++ precache-manifest.80a441ce0f8cbf400c74960b6faef4d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3172ecf22a3d7c56705b839d9e99d4da",
+    "url": "/countItDown/index.html"
+  },
+  {
+    "revision": "cfd8dea7380c1bf753c2",
+    "url": "/countItDown/static/css/2.965ebd84.chunk.css"
+  },
+  {
+    "revision": "0b5762a0bc4b315b7723",
+    "url": "/countItDown/static/css/main.b759fd9b.chunk.css"
+  },
+  {
+    "revision": "cfd8dea7380c1bf753c2",
+    "url": "/countItDown/static/js/2.d60098e8.chunk.js"
+  },
+  {
+    "revision": "0b5762a0bc4b315b7723",
+    "url": "/countItDown/static/js/main.a904d2a8.chunk.js"
+  },
+  {
+    "revision": "3d399e7f19c0139defbd",
+    "url": "/countItDown/static/js/runtime-main.44d90e2b.js"
+  },
+  {
+    "revision": "61eb5a4053f714f2194be9385aa2824d",
+    "url": "/countItDown/static/media/analog-watch-alarm_daniel-simion.61eb5a40.mp3"
+  }
+]);
